ca01cba59f189a076d49d0dc3f75b64264fe39d2
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-02-20 11:08:56 -0800

    Backup and clean house work

--- projects/Rebuttal_paper_simple_fit_mocks/plot_H0_hist.py
+++ projects/Rebuttal_paper_simple_fit_mocks/plot_H0_hist.py
@@ -12,7 +12,7 @@ import pickle
 import matplotlib as mat
 mat.rcParams['font.family'] = 'STIXGeneral'
 
-folder = 'MCsteps1000_10000/'
+folder = 'final_confirmed_MCsteps1000_10000/'
 #folder = 'MCsteps1000_10000_linear_False/'
 #folder = 'MCsteps[1000, 10000]_first_run_linear_True/'
 #folder = 'MCsteps1000_10000_second_run_linear_True/'
@@ -27,21 +27,22 @@ folder = 'MCsteps1000_10000/'
 #    mcmc_new_list, labels_new = trans_result
 #    H0_list = mcmc_new_list[:,-1]
 #    H0_lists.append(H0_list) 
-# pickle.dump([H0_lists], open(folder+'H0_SIE.pkl', 'wb'))         
-H0_lists = []
-for i in range(1,7):
-   result = pickle.load(open(folder+'sampler_results_SPEMD#{0}.pkl'.format(i),'rb'))
-   _, trans_result = result
-   mcmc_new_list, labels_new = trans_result
-   H0_list = mcmc_new_list[:,-1]
-   H0_lists.append(H0_list) 
-pickle.dump([H0_lists], open(folder+'H0_SPEMD.pkl', 'wb'))         
+# pickle.dump([H0_lists], open(folder+'H0_SIE.pkl', 'wb')) 
+        
+# H0_lists = []
+# for i in range(1,7):
+#    result = pickle.load(open(folder+'sampler_results_SPEMD#{0}.pkl'.format(i),'rb'))
+#    _, trans_result = result
+#    mcmc_new_list, labels_new = trans_result
+#    H0_list = mcmc_new_list[:,-1]
+#    H0_lists.append(H0_list) 
+# pickle.dump([H0_lists], open(folder+'H0_SPEMD.pkl', 'wb'))         
 
 #%%
 H0_true = 70.65595
 #model = 'SIE'
 #model = 'SPEMD'
-for model in ['SPEMD']:
+for model in ['SIE','SPEMD']:
     name = {'SIE'   :'SIE', 'SPEMD': "Power - law"}
     H0 = pickle.load(open(folder+'H0_{0}.pkl'.format(model),'rb'))[0]
     
@@ -49,7 +50,7 @@ for model in ['SPEMD']:
     for i in range(len(H0)):
         H0_l, H0_m, H0_h = np.percentile(H0[i],16,axis=0), np.median(H0[i]), np.percentile(H0[i],84,axis=0) 
         H0_sig = (H0_h-H0_l)/2
-        plt.hist(H0[i],normed=True, histtype=u'step',  label=(r'#{0}, H$_0$:{1:.1f}$\pm${2:.1f}'.format(i+1,H0_m, H0_sig)), linewidth = 2)
+        plt.hist(H0[i],density=True, histtype=u'step',  label=(r'#{0}, H$_0$:{1:.1f}$\pm${2:.1f}'.format(i+1,H0_m, H0_sig)), linewidth = 2)
     plt.plot(np.linspace(0,0.06)*0+np.median(H0_true) , np.linspace(0,0.06), linewidth = 4, color='black')
     
     plt.xlabel(r"H$_0$ [km s$^{-1}$ Mpc$^{-1}$]",fontsize=27)
--- projects/Rebuttal_paper_simple_fit_mocks/test_burning.py
+++ projects/Rebuttal_paper_simple_fit_mocks/test_burning.py
@@ -15,14 +15,15 @@ import matplotlib as mat
 import corner
 mat.rcParams['agg.path.chunksize'] = 10000
 
-folder = 'MCsteps1000_10000_PROFILE_terrible_result/'
+# folder = 'MCsteps1000_10000_1.3.0_non-linear_True_2ndrun/'
+folder = 'MCsteps100_100/'
 # folder = 'MCsteps1000_10000_1.3.0_non-linear_False/'
 
 model = 'SPEMD'
 # model = 'SIE'
 
-for i in range(2,3):
-    result = pickle.load(open(folder+'sampler_results_{1}#{0}.pkl'.format(i, model),'rb'))
+for i in range(1,2):
+    result = pickle.load(open(folder+'sampler_results_{1}#{0}.pkl'.format(i, model),'rb'),encoding="latin1") 
     fit_result, trans_result = result
     mcmc_new_list, labels_new = trans_result
 
@@ -32,7 +33,7 @@ chain_num = len(mcmc_new_list)
 #%%
 print(len(mcmc_new_list[1]),labels_new)
 
-plot = corner.corner(mcmc_new_list, labels=labels_new, show_titles=True, range= [[0.8,1.5],[1,3],[0,1],[0, 1],[2000,5000],[20,100]], 
+plot = corner.corner(mcmc_new_list, labels=labels_new, show_titles=True, #range= [[0.8,1.5],[1,3],[0,1],[0, 1],[2000,5000],[20,100]], 
                      quantiles=[0.16, 0.5, 0.84], #smooth = 0.8,
                      title_kwargs={"fontsize": 15}, label_kwargs = {"fontsize": 25},
                      levels=1.0 - np.exp(-0.5 * np.array([1.,2.]) ** 2))
