664be20c14cc386b17d43555d1e3e12baf108eea
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-03-14 19:20:43 -0700

    Modify dirzzle

--- projects/Sim_HST_JWST/HST_lensed_QSO/auto_drizzle_noqso.sh
+++ projects/Sim_HST_JWST/HST_lensed_QSO/auto_drizzle_noqso.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Run using iraf27 environment
-read -p 'Simulation ID:' id
-cp -r ../drizzle_temp/* sim_lens_ID_${id}
-cd sim_lens_ID_${id}
+read -p 'Simulation ID (noqso):' id
+cp -r ../drizzle_temp/* sim_lens_noqso_ID_${id}
+cd sim_lens_noqso_ID_${id}
 python header.py
 echo "cl<dri_img.cl; logout" | cl
 echo "cl<dri_psf.cl; logout" | cl
