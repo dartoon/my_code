c775fd0a81d50016a016857328a2a39e8ac20d41
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-03-16 00:27:26 -0700

    incorporate with F444W and some QSO templates, estimate AB mag looks too high

--- projects/Sim_HST_JWST/JWST_QSO/esti_F444w_magnitude/cal_F444W_mag.py
+++ projects/Sim_HST_JWST/JWST_QSO/esti_F444w_magnitude/cal_F444W_mag.py
@@ -33,17 +33,42 @@ def cal_filt_flam(array_spec, fil):
     filt_flam = int_flux/ int_filt   
     return filt_flam
 
-filename_p  = 'qso_template.fits' #Download from:http://www.stsci.edu/hst/instrumentation/reference-data-for-calibration-and-tools/astronomical-catalogs/the-agn-atlas
-#filename_p  = 'seyfert1_template.fits'
-#filename_p  = 'ngc1068_template.fits'
-hdul = pyfits.open(filename_p)
-table_spec = hdul[1].data
-name = hdul[1].columns
+#Tried
+###Download from:https://archive.stsci.edu/prepds/igm/ 
+#Note:Wavelength too short
 
-array_spec = np.zeros((len(table_spec), len(table_spec[0])))
-for i in range(len(table_spec)):
-    array_spec[i, :] = table_spec[i]
+##Download from:http://www.stsci.edu/hst/instrumentation/reference-data-for-calibration-and-tools/astronomical-catalogs/the-agn-atlas
+#filename_p  = 'stsci_temp/qso_template.fits' 
+##filename_p  = 'stsci_temp/seyfert1_template.fits'
+##filename_p  = 'stsci_temp/seyfert2_template.fits'
+##filename_p  = 'stsci_temp/ngc1068_template.fits'
+##filename_p  = 'stsci_temp/liner_template.fits'  #M81
+#hdul = pyfits.open(filename_p)
+#table_spec = hdul[1].data
+#name = hdul[1].columns
+#array_spec = np.zeros((len(table_spec), len(table_spec[0])))
+#for i in range(len(table_spec)):
+#    array_spec[i, :] = table_spec[i]
     
+
+#Download from SDSS: http://classic.sdss.org/dr5/algorithms/spectemplates/
+filename_p  = 'sdss_temp/spDR2-029.fit'  #QSO
+#filename_p  = 'sdss_temp/spDR2-030.fit'  #QSO with some BAL activity (QSO)
+#filename_p  = 'sdss_temp/spDR2-031.fit'  #QSO with some BAL activity (QSO)
+##filename_p  = 'sdss_temp/spDR2-032.fit'  #High-luminosity QSO (QSO) !!! Not work, no short Wavelen information
+hdul = pyfits.open(filename_p)
+spec_header = hdul[0].header
+spec_data = hdul[0].data
+wavelen_first_pi = spec_header['COEFF0']
+delta_wave = spec_header['COEFF1']
+array_spec = np.zeros((len(spec_data.T),2))
+array_spec[0,0]  = wavelen_first_pi
+for i in range(len(spec_data.T)-1):
+    array_spec[i+1,0] = array_spec[i, 0] + delta_wave
+array_spec[:,0] = 10** array_spec[:,0]      
+array_spec[:,1] = np.sum(spec_data, axis=0)
+array_spec = array_spec[100:]
+
 array_spec_temp = copy.deepcopy(array_spec)
 #%%Plot the Spec at the rest-frame
 array_spec = copy.deepcopy(array_spec_temp)
@@ -68,7 +93,7 @@ array_spec[:,0] *= (1+z)
 cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
 dl = cosmo.luminosity_distance(z=z).value  
 #m1450 = M1450 + 2.5*np.log10((dl*10**6/10)**2)   # Mv = m - 2.5 log[ (d/10)**2 ],  d in pc scale
-m1450 = M1450 + 5*(np.log10(dl*10**6)-1)
+m1450 = M1450 + 5*(np.log10(dl*10**6)-1) - 2.5*np.log10(1+z)
 lam = 1450 * (1+z)
 f_lambda_1450 = 10**(-0.4*(m1450+2.402+5.0*np.log10(lam)))
 idx = np.where( abs(array_spec[:,0] - lam) == np.min(abs(array_spec[:,0] - lam)) )[0][0]
@@ -89,8 +114,8 @@ plt.plot(z_fil[:,0], z_fil[:,1], label='HSC Z filter response', c='green')
 plt.plot(y_fil[:,0], y_fil[:,1], label='HSC Y filter response', c='orange')
 plt.scatter(lam, f_lambda_1450, c='r', marker="*", s =200, zorder = 10, label='observed M1450')
 
-HSC_z_lam = z_fil[np.where(z_fil[:,1] == z_fil[:,1].max())[0][0],0]
-HSC_y_lam = y_fil[np.where(y_fil[:,1] == y_fil[:,1].max())[0][0],0]
+HSC_z_lam = 8908.2
+HSC_y_lam = 9775.1
 
 z_filt_flam = cal_filt_flam(array_spec, z_fil)
 plt.scatter(HSC_z_lam, z_filt_flam, c='green')
@@ -103,16 +128,36 @@ obs_y_filt_flam = 10**(-0.4*(m_yAB+2.402+5.0*np.log10(HSC_y_lam)))
 plt.scatter(HSC_z_lam, obs_z_filt_flam, c='green', marker="*", s =200, zorder = 10, label='observed z_mag')
 plt.scatter(HSC_y_lam, obs_y_filt_flam, c='orange', marker="*", s =200, zorder = 10, label='observed y_mag')
 
+#Calculate F444 filter magnitude:
+f444w_fil = np.loadtxt('/Users/Dartoon/Astro/Packages/gsf/gsf/example/filter/444.fil')
+f444w_fil = f444w_fil[:,1:]
+f444w_fil[:,1] = f444w_fil[:,1]/f444w_fil[:,1].max()* array_spec[:,1].max()/6
+plt.plot(f444w_fil[:,0], f444w_fil[:,1], label='JWST F444W filter response', c='firebrick')
+f444w_lam = np.median(f444w_fil[:,0])
+f444w_filt_lam = cal_filt_flam(array_spec, f444w_fil) 
+f444w_mag = -2.5 * np.log10(f444w_filt_lam ) - 2.402 - 5.0 * np.log10(f444w_lam)
+
 plt.tick_params(labelsize=15)
 plt.xlabel("A",fontsize=27)
 plt.ylabel(r"f$_\lambda$ erg/s/cm$^2$/A",fontsize=27)
-plt.xlim(5000, 20000)
+plt.xlim(800, 60000)
+plt.ylim(10**-19.5,10**-17.5)
+#plt.ylim(10**-19.5,10**-17.5)
+#plt.yscale('log',basey=10) 
 plt.legend(prop={'size':15})
-
 plt.show()
 
-z_mag = -2.5 * np.log10(z_filt_flam ) - 2.402 - 5.0 * np.log10(z_fil[np.where(z_fil[:,1] == z_fil[:,1].max())[0][0],0])
-y_mag = -2.5 * np.log10(y_filt_flam ) - 2.402 - 5.0 * np.log10(y_fil[np.where(y_fil[:,1] == y_fil[:,1].max())[0][0],0])
+
+z_mag = -2.5 * np.log10(z_filt_flam ) - 2.402 - 5.0 * np.log10(HSC_z_lam)
+y_mag = -2.5 * np.log10(y_filt_flam ) - 2.402 - 5.0 * np.log10(HSC_y_lam)
+
+print("Input:")
+print("m_zAB", m_zAB)
+print("m_yAB", m_yAB)
+print("M_1450", round(M1450,3))
+
+print("================")
+print("Estimate")
 print("z_mag", round(z_mag,3))
 print("y_mag", round(y_mag,3))
-print("M_1450", round(M1450,3))
+print('F444W_mag', round(f444w_mag,3))
