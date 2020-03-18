0a87bd20eeea049f14a5b86d07b421bc9a9eb0c5
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-03-17 21:00:47 -0700

    Update nordri.py

--- projects/Sim_HST_JWST/JWST_QSO/drizzle_temp/nordri.py
+++ projects/Sim_HST_JWST/JWST_QSO/drizzle_temp/nordri.py
@@ -8,24 +8,24 @@ nimage = len(glob.glob('non_drizzled-image-*.fits'))
 filename='drimage.fits'
 im = pyfits.open(filename)[0].data.copy()
 im /=nimage
-pyfits.PrimaryHDU(im).writeto('Drz_QSO_image.fits',overwrite=True)
+pyfits.PrimaryHDU(im).writeto('Drz_QSO_image.fits')#,overwrite=True)
 
 filename='dripsf.fits'
 psf = pyfits.open(filename)[0].data.copy()
 psf /= np.sum(psf)  
-pyfits.PrimaryHDU(psf).writeto('Drz_PSF.fits',overwrite=True)
+pyfits.PrimaryHDU(psf).writeto('Drz_PSF.fits')#,overwrite=True)
 
 filename='drAGNclean.fits'
 AGN = pyfits.open(filename)[0].data.copy()
 AGN /= nimage
-pyfits.PrimaryHDU(AGN).writeto('Drz_AGNclean_image.fits',overwrite=True)
+pyfits.PrimaryHDU(AGN).writeto('Drz_AGNclean_image.fits')#,overwrite=True)
 
 filename='drHOSTclean.fits'
 HOST = pyfits.open(filename)[0].data.copy()
 HOST /=nimage
-pyfits.PrimaryHDU(HOST).writeto('Drz_HOSTclean_image.fits',overwrite=True)
+pyfits.PrimaryHDU(HOST).writeto('Drz_HOSTclean_image.fits')#,overwrite=True)
 
 filename='drPOINTclean.fits'
 POINT = pyfits.open(filename)[0].data.copy()
 POINT /=nimage
-pyfits.PrimaryHDU(POINT).writeto('Drz_POINTclean_image.fits',overwrite=True)
+pyfits.PrimaryHDU(POINT).writeto('Drz_POINTclean_image.fits')#,overwrite=True)
