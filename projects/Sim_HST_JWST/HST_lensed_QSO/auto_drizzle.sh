db07768517348481bed806eebc443f7bd5559c2a
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-03-27 19:30:47 -0700

    Simulate 21 system, seed from 501 to 521.

--- projects/Sim_HST_JWST/HST_lensed_QSO/auto_drizzle.sh
+++ projects/Sim_HST_JWST/HST_lensed_QSO/auto_drizzle.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Run using iraf27 environment
-read -p 'Simulation ID:' id
+# read -p 'Simulation ID:' id
+for id in 501 502 503 504 505 506 507 508 509 510 511 512 513 514 515 516 517 518 519 520 521
+do
 cp -r ../drizzle_temp/* sim_lens_ID_${id}
 cd sim_lens_ID_${id}
 python header.py
@@ -11,3 +13,4 @@ rm -r uparm
 rm *.cl *.py
 rm *w.fits
 rm dri*.fits
+done
--- projects/Sim_HST_JWST/HST_lensed_QSO/auto_drizzle_noqso.sh
+++ projects/Sim_HST_JWST/HST_lensed_QSO/auto_drizzle_noqso.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Run using iraf27 environment
+for id in 501 502 503 504 505 506 507 508 509 510 511 512 513 514 515 516 517 518 519 520 521
+do
 read -p 'Simulation ID (noqso):' id
 cp -r ../drizzle_temp/* sim_lens_noqso_ID_${id}
 cd sim_lens_noqso_ID_${id}
@@ -11,3 +13,4 @@ rm -r uparm
 rm *.cl *.py
 rm *w.fits
 rm dri*.fits
+done
