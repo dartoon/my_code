4de3b107898c5d81a51d9f13028c81ceb4891d30
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-03-27 19:45:45 -0700

    Update nordri.py

--- projects/Sim_HST_JWST/drizzle_temp/nordri.py
+++ projects/Sim_HST_JWST/drizzle_temp/nordri.py
@@ -6,9 +6,9 @@ nimage=8.
 im = pyfits.open(filename)[0].data.copy()
 #scaling back to cps
 im /=nimage
-pyfits.PrimaryHDU(im).writeto('Drz_QSO_image.fits',overwrite=True)
+pyfits.PrimaryHDU(im).writeto('Drz_QSO_image.fits')#,overwrite=True)
 
 filename='dripsf.fits'
 psf = pyfits.open(filename)[0].data.copy()
 psf /= np.sum(psf)  
-pyfits.PrimaryHDU(psf).writeto('Drz_PSF.fits',overwrite=True)
+pyfits.PrimaryHDU(psf).writeto('Drz_PSF.fits')#,overwrite=True)
