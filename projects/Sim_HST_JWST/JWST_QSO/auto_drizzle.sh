1aeac4cf7aa44683023c39933543ca2fe4f25270
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-03-30 16:28:17 -0700

    Revert "Ready to auto simulation for JWST quasars"
    This reverts commit 9d72b11196b076cabfcd97e9e9a284f7b1e0fd22.

--- projects/Sim_HST_JWST/JWST_QSO/auto_drizzle.sh
+++ projects/Sim_HST_JWST/JWST_QSO/auto_drizzle.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 #Run using iraf27 environment
 read -p 'Drizzling filter:' filt
-#read -p 'Simulation ID:' id
-for seed in 0
-do
-for id in 1 2 3 4 5
-do
-# echo sim_ID${id}_${filt}_seed${seed}
-cp -r drizzle_temp_${filt}/* sim_ID${id}_${filt}_seed${seed}
-cd sim_ID${id}_${filt}_seed${seed}
+read -p 'Simulation ID:' id
+cp -r ${filt}_drizzle_temp/* sim_ID_${id}
+cd sim_${filt}_ID_${id}
 python header.py
 echo "cl<dri_img.cl; logout" | cl
 echo "cl<dri_AGNclean.cl; logout" | cl
@@ -19,5 +14,3 @@ rm -r uparm
 rm *.cl *.py
 rm *w.fits
 rm dr*.fits
-done
-done
