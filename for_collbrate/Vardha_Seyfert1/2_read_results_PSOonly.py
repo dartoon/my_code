98373cf8ce67f94f0672ce7636291e9cebc926a5
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-04-15 11:23:52 -0700

    Update 2_read_results_PSOonly.py

--- for_collbrate/Vardha_Seyfert1/2_read_results_PSOonly.py
+++ for_collbrate/Vardha_Seyfert1/2_read_results_PSOonly.py
@@ -15,7 +15,7 @@ import copy
 
 picklename = 'result_vardha/PSO_only/l24_sersicdisk.pkl'
 picklename = 'result_vardha/l5548_sersicdisk.pkl'
-
+picklename = 'result_vardha/l79_sersicdiskbar.pkl'
 
 result = pickle.load(open(picklename,'rb'))
 [best_fit,pso_fit,mcmc_fit, trans_paras] = result
@@ -24,8 +24,8 @@ source_result, image_host, ps_result, image_ps, _ =best_fit
 chain_list, param_list, _ = pso_fit
 samples_mcmc, param_mcmc, dist_mcmc, _ = mcmc_fit
 
-print "best-fit source_result:", source_result
-print "best-fit ps_result:", ps_result
+print("best-fit source_result:", source_result)
+print("best-fit ps_result:", ps_result)
 pix_sz = 0.04
 
 #%%diagnose the PSO chain convergency
@@ -36,4 +36,4 @@ for i in range(len(chain_list)):
         f, axes = out_plot.plot_chain(chain_list[i], param_list[i])
 plt.show()
 
-print 'inferred host flux', [image_host[i].sum() for i in range(len(image_host))]
+print('inferred host flux', [image_host[i].sum() for i in range(len(image_host))])
