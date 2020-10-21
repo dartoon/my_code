6c3e88b48b3c036233b576c5ddefab4ad0bdc2bb
dartoon <dingxuheng@mail.bnu.edu.cn>
2020-10-21 15:23:15 +0800

    Update example template

--- package_code/decomprofile_example/HST_QSO_model_example.py
+++ package_code/decomprofile_example/HST_QSO_model_example.py
@@ -28,9 +28,9 @@ exp_map = exp * wht/mean_wht
 
 #%%Start to use decomprofile
 from decomprofile.data_process import DataProcess
-data_process = DataProcess(fov_image = fov_image, target_pos = [1135, 648], header = header,
+data_process = DataProcess(fov_image = fov_image, target_pos = [1142., 637.], header = header,
                           rm_bkglight = False, exptime = exp_map, if_plot=False, zp = 27.0)
-data_process.generate_target_materials(radius=30, create_mask = False, nsigma=2.8,
+data_process.generate_target_materials(radius=55, create_mask = False, nsigma=2.8,
                                       exp_sz= 1.2, npixels = 15, if_plot=False)
 
 #%%PSF works.
@@ -44,21 +44,23 @@ data_process.checkout() #Check if all the materials is known.
 #%%Start to produce the class and params for lens fitting.
 from decomprofile.fitting_specify import FittingSpeficy
 fit_sepc = FittingSpeficy(data_process)
-fit_sepc.prepare_fitting_seq(point_source_num = 1, fix_n_list= [[0,4]], fix_center_list = [[0,0]])
-
+# fit_sepc.prepare_fitting_seq(point_source_num = 1, fix_n_list= [[0,4]], fix_center_list = [[0,0]])
+fit_sepc.prepare_fitting_seq(point_source_num = 1)
 #Plot the initial settings for fittings. 
 fit_sepc.plot_fitting_sets()
+
+del fit_sepc.kwargs_model['source_light_model_list']  #Remove all the aperture and fit only PS.
 
 fit_sepc.build_fitting_seq()
 
 #%%Setting the fitting method and run.
 from decomprofile.fitting_process import FittingProcess
-fit_run = FittingProcess(fit_sepc, savename = 'test_fix_n_4')
+fit_run = FittingProcess(fit_sepc, savename = 'test_no_host')
 fit_run.run()
 fit_run.plot_all()
 fit_run.dump_result()
-#print(fit_run.final_galaxy_result[0])
-#
+print(fit_run.final_galaxy_result[0])
+
 ##%%
 ## Test load pkl
 #import pickle
---
+++ package_code/decomprofile_example/Read_pkl.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 19:49:11 2020
+
+@author: Xuheng Ding
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.io.fits as pyfits
+
+
+# Test load pkl
+import pickle
+picklename = 'HSC_dualAGN_PSPS.pkl'
+fitting_run_class = pickle.load(open(picklename,'rb'))
+fitting_run_class.plot_all()
+fitting_run_class.run_diag()
+
