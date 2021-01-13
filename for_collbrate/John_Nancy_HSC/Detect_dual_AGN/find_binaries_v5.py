5045a379a2d2ff0225cdaecfcd57e55250ace514
dartoon <dingxuheng@mail.bnu.edu.cn>
2021-01-13 14:48:28 +0900

    Update and add find dual with modelling 5 bands.

--- for_collbrate/John_Nancy_HSC/Detect_dual_AGN/find_binaries_v5.py
+++ for_collbrate/John_Nancy_HSC/Detect_dual_AGN/find_binaries_v5.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov 16 11:19:14 2018
@@ -18,21 +18,40 @@ from decomprofile.data_process import DataProcess
 from decomprofile.fitting_specify import FittingSpeficy
 from decomprofile.fitting_process import FittingProcess
 
-#image_ID = sys.argv[1] #'141637.44+003352.2' 
-#image_RA = float(sys.argv[2]) #214.15602111816406
-#image_DEC = float(sys.argv[3]) #0.5645210146903992
+# image_ID = sys.argv[1] #'141637.44+003352.2' 
+# image_RA = float(sys.argv[2]) #214.15602111816406
+# image_DEC = float(sys.argv[3]) #0.5645210146903992
 
-image_ID ='100043.13+020637.2' 
-image_RA = 150.1797789
-image_DEC = 2.110369603
+# image_ID ='100043.13+020637.2' 
+# image_RA = 150.1797789
+# image_DEC = 2.110369603
 
+# image_ID ='010048.81+021604.0'
+# image_RA = 15.2033809 
+# image_DEC = 2.2677925
+
+image_ID ='141637.44+003352.2' 
+image_RA = 214.156021
+image_DEC = 0.564521
+
+# # run_line = [5069, 5070, 5086, 5093, 5096, 5110][1]
+# run_line = 0
+# targets_info = open("QSO_id_file.txt", "r")
+# targets_info = targets_info.read().split('\n')
+# info = copy.deepcopy(targets_info[run_line])
+# info = info.split(' ')
+# info = [info[i] for i in range(len(info)) if info[i] != '']
+# image_ID, image_RA, image_DEC = info[:3]
+# image_RA, image_DEC = float(image_RA), float(image_DEC)
+
+#%%
 print(image_ID, image_RA, image_DEC)
 
 deep_seed = True  #Set as True to put more seed and steps to fit,
-show_plot = 1
-fit_data = False  #If you simply want to do the search without fitting, set False
+show_plot = 0
+fit_data = True  #If you simply want to do the search without fitting, set False
 
-image_folder = './images_directory/'
+image_folder = '../images_directory/'
     
 if os.path.exists('fit_result_detect')==False:
     os.mkdir('fit_result_detect')
@@ -41,11 +60,10 @@ filename_ascii = 'RESULTS/' + image_ID + '_result.txt'
 
 # band_run_list = [2,0,1,3,4]  #run I band first
 # band_seq = ['G', 'R', 'I', 'Z', 'Y']
-band_run_list = [0]  #run I band first
+# band_run_list = [0]  #run I band first
 band_seq = ['I']
-
-filename_list = [image_ID+'_HSC-{0}.fits'.format(band_seq[i]) for i in range(len(band_run_list))]
-run_list = copy.deepcopy(band_run_list)
+filename_list = [image_ID+'_HSC-{0}.fits'.format(band_seq[i]) for i in range(len(band_seq))]
+run_list = [0]
 
 data_process_list, zp_list = [], []
 
@@ -62,8 +80,8 @@ for i in range(len(band_seq)):
         err_data= fitsFile[3].data ** 0.5
         
         file_header0 = fitsFile[0].header
-        FLUXMAG0 = file_header0['FLUXMAG0']
-        zp =  2.5 * np.log10(FLUXMAG0)   # This is something Xuheng can't make sure.
+        # FLUXMAG0 = file_header0['FLUXMAG0']
+        zp =  27.0 #2.5 * np.log10(FLUXMAG0)   # This is something Xuheng can't make sure.
         
         data_process_i = DataProcess(fov_image = fov_image, fov_noise_map = err_data, target_pos = [image_RA, image_DEC],
                                    pos_type = 'wcs', header = header,
@@ -73,7 +91,7 @@ for i in range(len(band_seq)):
                                               exp_sz= 1.2, npixels = 15, if_plot=False)        
         PSF = pyfits.getdata(image_folder+filename_list[i].split('.fits')[0]+'_psf.fits')
         if len(PSF) != 0 and PSF.shape[0] != PSF.shape[1]:
-            cut = ((PSF.shape[0] - PSF.shape[1])/2)
+            cut = int((PSF.shape[0] - PSF.shape[1])/2)
             if cut>0:
                 PSF = PSF[cut:-cut,:]
             elif cut<0:
@@ -87,63 +105,68 @@ for i in range(len(band_seq)):
 
 #%%
 for k in run_list:  #['G', 'R', 'I', 'Z', 'Y']
-    print("Fiting the: "+ filename_list[k])
-    if_dual = False
     QSO_img = data_process_list[k].target_stamp
-    x, y = find_loc_max(QSO_img, neighborhood_size = 3, threshold = 1)
+    x, y = find_loc_max(QSO_img, neighborhood_size = 4, threshold = 4)
     arr_x, arr_y = np.asarray(x, dtype=float), np.asarray(y, dtype=float)
     center = len(QSO_img)/2
     bool_x, bool_y = (arr_x>(center-18))*(arr_x<(center+18)), (arr_y>(center-18))*(arr_y<(center+18))
     arr_x = arr_x[bool_x*bool_y]
     arr_y = arr_y[bool_x*bool_y]
+    #Remove arr_x's elememt if the corresponding pixel is too faint:
+    arr_bool = np.array([True] * len(arr_x), dtype=bool)
+    for i in range(len(arr_x)):
+        if QSO_img[int(arr_y[i]), int(arr_x[i])] < 3.0 :
+            arr_bool[i] = False
+    arr_x = arr_x[arr_bool]
+    
     qsoid = filename_list[k].split('.fits')[0]
+    claim = ''
     if len(arr_x)>=2:
-        if_dual = True
-        claim = "This {0} is likely to be a {1} system (based on multi-peaks)!!!".format(filename_list[k], 'BH'*len(arr_x))
-    elif len(arr_x)==1:
-        twoD_Gau_p_PSF =  fit_data_twoD_Gaussian(data_process_list[k].PSF_list[0])
-        frz = int(center/2)
-        twoD_Gau_p_data = fit_data_twoD_Gaussian(QSO_img[frz:-frz,frz:-frz])
-        q_PSF = twoD_Gau_p_PSF[3]/twoD_Gau_p_PSF[4]
-        q_PSF = min(q_PSF, 1/q_PSF)
-        q_data = twoD_Gau_p_data[3]/twoD_Gau_p_data[4]
-        q_data = min(q_data, 1/q_data)
-        if abs((q_data-q_PSF)/q_PSF) > 0.15 :   #!!! Set the level as 15% mismatch to PSF
-            if_dual = True
-            claim = "This {0} is likely to have closed dual AGN pair (based on FWHM)!!!".format(filename_list[k])
-
-    if if_dual == True and os.path.exists('fit_result_detect/{0}/'.format(qsoid))==False:
-        os.mkdir('fit_result_detect/{0}/'.format(qsoid))
-        if len(arr_x)==1:
-            fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.5, 10))
-            ax1.imshow(QSO_img[frz:-frz,frz:-frz], origin='lower', cmap='gist_heat', norm=LogNorm(), vmin=1.e-4 , vmax = np.max(QSO_img) )
-            ax1.set_title('Data')
-            QSO_2D_fitted = twoD_Gaussian(len(QSO_img[frz:-frz,frz:-frz]), *twoD_Gau_p_data)
-            ax2.imshow(QSO_2D_fitted.reshape(len(QSO_img[frz:-frz,frz:-frz]), len(QSO_img[frz:-frz,frz:-frz])), origin='lower', cmap='gist_heat', norm=LogNorm(), vmin=1.e-4 , vmax = np.max(QSO_img) )
-            ax2.set_title('fitted Gaussian Image')
-            plt.savefig('fit_result_detect/{0}/proof-2close.pdf'.format(qsoid))
-            if show_plot == 1:
-                plt.show()
-            else:
-                plt.close()
+        if os.path.exists('fit_result_detect/{0}/'.format(qsoid))==False:
+            os.mkdir('fit_result_detect/{0}/'.format(qsoid))
+        claim = claim+"\nThis {0} is likely to be a {1} system (based on multi-peaks)!!!".format(filename_list[k], 'BH'*len(arr_x))
+        plt.imshow(QSO_img, origin='lower', norm=LogNorm())
+        for i in range(len(arr_x)):
+            plt.text(arr_x[i], arr_y[i],'BH{0}'.format(i))
+            plt.plot(arr_x[i], arr_y[i],'ro')
+        plt.savefig('fit_result_detect/{0}/proof-BHBH.pdf'.format(qsoid))
+        if show_plot == 1:
+            plt.show()
         else:
-            plt.imshow(QSO_img, origin='low', norm=LogNorm())
-            for i in range(len(arr_x)):
-                plt.text(arr_x[i], arr_y[i],'BH{0}'.format(i))
-                plt.plot(arr_x[i], arr_y[i],'ro')
-            plt.savefig('fit_result_detect/{0}/proof-BHBH.pdf'.format(qsoid))
-            if show_plot == 1:
-                plt.show()
-            else:
-                plt.close()
-            
-    if if_dual == True and fit_data == True:
+            plt.close()
+
+    #Fit central as twoD Gaussian Anyway.            
+    twoD_Gau_p_PSF =  fit_data_twoD_Gaussian(data_process_list[k].PSF_list[0])
+    frz = int(center/2)
+    twoD_Gau_p_data = fit_data_twoD_Gaussian(QSO_img[frz:-frz,frz:-frz])
+    q_PSF = twoD_Gau_p_PSF[3]/twoD_Gau_p_PSF[4]
+    q_PSF = min(q_PSF, 1/q_PSF)
+    q_data = twoD_Gau_p_data[3]/twoD_Gau_p_data[4]
+    q_data = min(q_data, 1/q_data)
+    if abs((q_data-q_PSF)/q_PSF) > 0.15 :   #!!! Set the level as 15% mismatch to PSF
+        if os.path.exists('fit_result_detect/{0}/'.format(qsoid))==False:
+            os.mkdir('fit_result_detect/{0}/'.format(qsoid))
+        claim = claim+"\nThis {0} is also likely to have closed dual AGN pair (based on FWHM)!!!".format(filename_list[k])
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12.5, 10))
+        ax1.imshow(QSO_img[frz:-frz,frz:-frz], origin='lower', cmap='gist_heat', norm=LogNorm(), vmin=1.e-4 , vmax = np.max(QSO_img) )
+        ax1.set_title('Data')
+        QSO_2D_fitted = twoD_Gaussian(len(QSO_img[frz:-frz,frz:-frz]), *twoD_Gau_p_data)
+        ax2.imshow(QSO_2D_fitted.reshape(len(QSO_img[frz:-frz,frz:-frz]), len(QSO_img[frz:-frz,frz:-frz])), origin='lower', cmap='gist_heat', norm=LogNorm(), vmin=1.e-4 , vmax = np.max(QSO_img) )
+        ax2.set_title('fitted Gaussian Image')
+        plt.savefig('fit_result_detect/{0}/proof-2close.pdf'.format(qsoid))
+        if show_plot == 1:
+            plt.show()
+        else:
+            plt.close()
+
+    if os.path.exists('fit_result_detect/{0}/'.format(qsoid)) == True and fit_data == True:
+        print("Fiting the: "+ filename_list[k])
         print(claim)
         print("Comparing the fitting Chisq:")
         write_result =  open('fit_result_detect/{0}/fit_result.txt'.format(qsoid),'w') 
         write_result.write("#The fitting information:\n")
         #==============================================================================
-        # fitting the QSO as a BH + Sersic       
+        # fitting the QSO as a BH (PS) + Sersic       
         #==============================================================================
         for ft in range(1):     #The fitting rounds for each band
             print("fitting the QSO as one BH + Sersic ")
@@ -169,22 +192,22 @@ for k in run_list:  #['G', 'R', 'I', 'Z', 'Y']
             tag_name = tag + "_qso_final_plot"
             print(call("mv {0} {1}".format(tag_name+'.pdf', tag+"_chisq_"+repr(round(reduced_Chisq_0,1)))+'.pdf', shell=True))
         #==============================================================================
-        # fitting the QSO as a BHBH        
+        # fitting the QSO as a BHBH (PS+PS)        
         #==============================================================================
         for ft in range(1):
             print("fitting the QSO as {0} point sources".format(len(arr_x)))
             num_BHBH = max(len(arr_x), 2)
             fit_time = ft #len(glob.glob("fit_result_detect/{0}/fit_image_*_SB_profile_annuli*.pdf".format(file_name_seq[k])))
             tag = 'fit_result_detect/{0}/fit_image1_PSPS_fittime-{1}'.format(qsoid,fit_time+1)
-            _fit_sepc = FittingSpeficy(data_process_list[k])
-            del _fit_sepc.apertures[0]
-            _fit_sepc.prepare_fitting_seq(point_source_num = num_BHBH)
-            _fit_sepc.build_fitting_seq()
-            _fit_run = FittingProcess(_fit_sepc, savename = tag)
-            _fit_run.run(algorithm_list = ['PSO'], setting_list= [None]) 
-            _fit_run.translate_result()
-            _fit_run.plot_final_qso_fit(target_ID = qsoid, save_plot = True, show_plot = show_plot)            
-            source_result_1, ps_result_1 = _fit_run.final_result_galaxy, _fit_run.final_result_ps 
+            _fit_sepc_1 = FittingSpeficy(data_process_list[k])
+            del _fit_sepc_1.apertures[0]
+            _fit_sepc_1.prepare_fitting_seq(point_source_num = num_BHBH)
+            _fit_sepc_1.build_fitting_seq()
+            _fit_run_1 = FittingProcess(_fit_sepc_1, savename = tag)
+            _fit_run_1.run(algorithm_list = ['PSO'], setting_list= [None]) 
+            _fit_run_1.translate_result()
+            _fit_run_1.plot_final_qso_fit(target_ID = qsoid, save_plot = True, show_plot = show_plot)            
+            source_result_1, ps_result_1 = _fit_run_1.final_result_galaxy, _fit_run_1.final_result_ps 
             AGN_mags = [ps_result_1[i]['magnitude'] for i in range(len(ps_result_1))]
             if len(ps_result_1) == 2:
                 c_miss = np.sqrt((ps_result_1[0]['ra_image']-ps_result_1[1]['ra_image'])**2+(ps_result_1[0]['dec_image']-ps_result_1[1]['dec_image'])**2)
@@ -208,21 +231,21 @@ for k in run_list:  #['G', 'R', 'I', 'Z', 'Y']
             tag_name = tag + "_qso_final_plot"  
             print(call("mv {0} {1}".format(tag_name+'.pdf', tag+"_chisq_"+repr(round(reduced_Chisq_1,1)))+'.pdf', shell=True))
         #==============================================================================
-        # fitting the QSO as a BHBH + Sersic       
+        # fitting the QSO as a BHBH (PS+PS) + Sersic       
         #==============================================================================
         for ft in range(1):
             print("fitting the QSO as {0} point sources + Sersic".format(len(arr_x)))
             num_BHBH = max(len(arr_x), 2)
             fit_time = ft
             tag = 'fit_result_detect/{0}/fit_image2_PSPS+Sersic_fittime-{1}'.format(qsoid,fit_time+1)
-            _fit_sepc = FittingSpeficy(data_process_list[k])
-            _fit_sepc.prepare_fitting_seq(point_source_num = num_BHBH)
-            _fit_sepc.build_fitting_seq()
-            _fit_run = FittingProcess(_fit_sepc, savename = tag)
-            _fit_run.run(algorithm_list = ['PSO'], setting_list= [None]) 
-            _fit_run.translate_result()
-            _fit_run.plot_final_qso_fit(target_ID = qsoid, save_plot = True, show_plot = show_plot)     
-            source_result_2, ps_result_2 = _fit_run.final_result_galaxy, _fit_run.final_result_ps 
+            _fit_sepc_2 = FittingSpeficy(data_process_list[k])
+            _fit_sepc_2.prepare_fitting_seq(point_source_num = num_BHBH)
+            _fit_sepc_2.build_fitting_seq()
+            _fit_run_2 = FittingProcess(_fit_sepc_2, savename = tag)
+            _fit_run_2.run(algorithm_list = ['PSO'], setting_list= [None]) 
+            _fit_run_2.translate_result()
+            _fit_run_2.plot_final_qso_fit(target_ID = qsoid, save_plot = True, show_plot = show_plot)     
+            source_result_2, ps_result_2 = _fit_run_2.final_result_galaxy, _fit_run_2.final_result_ps 
             host_mag = source_result_2[0]['magnitude']
             AGN_mags = [ps_result_2[i]['magnitude'] for i in range(len(ps_result_2))]
             if len(ps_result_2) == 2:
