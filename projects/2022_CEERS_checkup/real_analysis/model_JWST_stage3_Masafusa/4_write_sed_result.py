3d2fd21a4778a03769d9433ee18c7151f17f3630
dartoon <dingxuheng@mail.bnu.edu.cn>
2022-09-02 13:31:57 +0900

    Update 4_write_sed_result.py

--- projects/2022_CEERS_checkup/real_analysis/model_JWST_stage3_Masafusa/4_write_sed_result.py
+++ projects/2022_CEERS_checkup/real_analysis/model_JWST_stage3_Masafusa/4_write_sed_result.py
@@ -10,7 +10,7 @@ import numpy as np
 import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 import glob
-write_file =  open('sed_2d_result.txt','w')
+write_file =  open('sed_2d_result_bin4.txt','w')
 write_file.write("count_i, smass, sfr, m_age, l_age, AV \n")
 steller_files = glob.glob('esti_smass/20220830*/SFH_*.fits')
 
