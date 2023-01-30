382c3ba994d23e7c914d23182a0c1eef25f7d1a3
dartoon <dingxuheng@mail.bnu.edu.cn>
2023-01-30 11:50:27 +0900

    Update 1_run_fit_onidark.py

--- projects/2022_COSMOSweb/1_run_fit_onidark.py
+++ projects/2022_COSMOSweb/1_run_fit_onidark.py
@@ -11,8 +11,9 @@ import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 import pickle,glob
 
-import sys
-count = int(sys.argv[1]) - 1 # 1 - 12586
+# import sys
+# count = int(sys.argv[1]) - 1 # 1 - 12586
+count = 1
 
 # filt_i = 1
 # filt = ['F115W', 'F150W','F277W', 'F444W'][filt_i]
@@ -21,10 +22,10 @@ cata_list = pickle.load(open('material/cata_list.pkl','rb'))
 fit_files = glob.glob('fit_material/fit_notrunyet_*.pkl')
 fit_files.sort()
 
-# i = count
-# fit_run = pickle.load(open(fit_files[i],'rb'))
-# # Quick run:
-# fit_run.run(algorithm_list = ['PSO'], fitting_level=['norm'])
-# fit_run.plot_final_qso_fit(target_ID =None, show_plot=False)
-# savename = fit_files[i].replace('_notrunyet_', '_run_')[:-4]+'_{0}.pkl'.format(i)
-# pickle.dump(fit_run , open(savename, 'wb'))
+i = count
+fit_run = pickle.load(open(fit_files[i],'rb'))
+fit_run.run(algorithm_list = ['PSO','PSO','PSO'], fitting_level=['norm', 'deep','deep'])
+fit_run.plot_final_qso_fit(target_ID =None, show_plot=False)
+savename = fit_files[i].replace('_notrunyet_', '_run_')
+savename = savename.replace('fit_material', 'fit_result')
+pickle.dump(fit_run , open(savename, 'wb'))
