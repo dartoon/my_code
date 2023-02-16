503232547a197734c852ce0a292503c6b25e25a9
dartoon <dingxuheng@mail.bnu.edu.cn>
2023-02-16 14:17:12 +0900

    Update 2_write_2d_info.py

--- projects/2022_COSMOSweb/2d_sed/2_write_2d_info.py
+++ projects/2022_COSMOSweb/2d_sed/2_write_2d_info.py
@@ -23,12 +23,12 @@ for name in ['cid_473', 'cid_1210', 'cid_1245']:
         m_age = info['T_MW_50']
         l_age = info['T_LW_50']
         AV = info['AV_50']
-        # shdul = pyfits.open(file.replace('SFH','gsf_spec'))
-        # info_stellar = shdul[1].header 
-        # BV50 = info_stellar['BV50']
-        f = open( file.split('SFH')[0]+'gsf_spec_header.txt',"r")
-        string = f.read()
-        BV50 = float(string.split('BV50    =')[1].split('BV84')[0])
+        shdul = pyfits.open(file.replace('SFH','gsf_spec'))
+        info_stellar = shdul[1].header 
+        BV50 = info_stellar['BV50']
+        # f = open( file.split('SFH')[0]+'gsf_spec_header.txt',"r")
+        # string = f.read()
+        # BV50 = float(string.split('BV50    =')[1].split('BV84')[0])
         filename = name+'_sed_2d_result_bin2.txt'
         if_file = glob.glob(filename)
         if if_file == []:
