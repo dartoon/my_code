bbce76cf167f8f018db5e8be4393d318b52b2213
dartoon <dingxuheng@mail.bnu.edu.cn>
2023-02-27 12:50:01 +0900

    Update 3_write_sed_result.py

--- projects/2022_CEERS_checkup/real_analysis/2d_SED_AEGIS_683/3_write_sed_result.py
+++ projects/2022_CEERS_checkup/real_analysis/2d_SED_AEGIS_683/3_write_sed_result.py
@@ -11,7 +11,7 @@ import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 import glob
 write_file =  open('sed_2d_result_bin2.txt','w')
-write_file.write("count_i, smass, sfr, m_age, l_age, AV \n")
+write_file.write("count_i, smass, sfr, m_age, l_age, AV, E(B-V) \n")
 steller_files = glob.glob('esti_smass/20221213*/SFH_*.fits')
 
 steller_files.sort()
@@ -20,11 +20,15 @@ for steller_file in steller_files:
     hdul = pyfits.open(steller_file)
     count = steller_file.split('20221213')[1].split('/')[0]
     info = hdul[0].header 
+    f = open( steller_file.split('SFH')[0]+'gsf_spec_header.txt',"r")
+    string = f.read()
+    BV50 = float(string.split('BV50    =')[1].split('BV84')[0])
+    
     smass = info['Mstel_50']
     sfr = info['SFR_50']
     m_age = info['T_MW_50']
     l_age = info['T_LW_50']
     AV = info['AV_50']
-    write_file.write("{0} {1} {2} {3} {4} {5}".format(count, smass, sfr, m_age, l_age, AV))
+    write_file.write("{0} {1} {2} {3} {4} {5} {6:.4f}".format(count, smass, sfr, m_age, l_age, AV, BV50))
     write_file.write("\n")
 write_file.close()
