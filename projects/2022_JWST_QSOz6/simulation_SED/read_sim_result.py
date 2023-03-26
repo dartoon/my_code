ce3787815cc6456a8161dcf5044dd844492a325f
dartoon <dingxuheng@mail.bnu.edu.cn>
2023-03-26 19:01:31 +0900

    Update read_sim_result.py

--- projects/2022_JWST_QSOz6/simulation_SED/read_sim_result.py
+++ projects/2022_JWST_QSOz6/simulation_SED/read_sim_result.py
@@ -11,7 +11,8 @@ import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 import glob
 
-path = 'second_run/'
+# path = 'second_run/'
+path = 'third_run/'
 folder = path+'F150W_F356W/'  #F150W_F356W
 # folder = path+'F150W_F200W/'
 # folder = path+'F150W_F277W/'
@@ -22,7 +23,7 @@ folder = path+'F150W_F356W/'  #F150W_F356W
 
 result_folders = glob.glob(folder + 'seed*_result')
 
-sim_folder = 'second_run/simulation/'
+sim_folder = path + '/simulation/'
 smass_match = []
 age_match = []
 ages = []
@@ -36,7 +37,7 @@ for result_folder in result_folders:
     hdul = pyfits.open(steller_file)
     info1 = hdul[0].header 
     smass_True = float(info1['Mstel_50'])
-    sim_mass_up_low.append(float(info1['Mstel_84']) - float(info1['Mstel_16']) )
+    sim_mass_up_low.append(float(info1['Mstel_84']) - float(info1['Mstel_50']) )
     ages.append(10**float(info1['T_MW_50']))
     Avs.append(float(info1['AV_50']))
     
@@ -44,14 +45,14 @@ for result_folder in result_folders:
     hdul = pyfits.open(steller_file)
     info2 = hdul[0].header 
     smass_infer = float(info2['Mstel_50'])
-    mod_mass_up_low.append(float(info2['Mstel_84']) - float(info2['Mstel_16']) )
+    mod_mass_up_low.append(float(info2['Mstel_84']) - float(info2['Mstel_50']) )
     # print(float(smass_infer)-float(smass_True))
     smass_match.append(float(smass_infer)-float(smass_True))
-    # if len(seed) == 2:
-    # if seed == '22':
-    #     print(info1)
-    #     print(info2)
-    #     print(seed, float(smass_infer)-float(smass_True))
+    # if len(seed) == 1:
+    # # if seed == '7':
+    #     print(seed)
+    #     print(float(info1['Mstel_84']) - float(info1['Mstel_16']))
+    #     print(float(info2['Mstel_84']) - float(info2['Mstel_16']))
     age_match.append( 10**float(info2['T_MW_50'])-10**float(info1['T_MW_50']) )
     
 #%%
@@ -62,14 +63,20 @@ sim_mass_up_low = np.array(sim_mass_up_low)
 mod_mass_up_low = np.array(mod_mass_up_low)
 comb= folder.split('/')[-2]
 
+
 plt.figure(figsize=(7,5))
-eff_bool = (sim_mass_up_low<1.2)*(mod_mass_up_low<1.2)
+eff_bool = (sim_mass_up_low<100)
+# eff_bool = (sim_mass_up_low<0.4) *(mod_mass_up_low<0.4) #* (sim_mass_up_low>0.15) *(mod_mass_up_low>0.15)
+eff_bool = (sim_mass_up_low<np.percentile(sim_mass_up_low,70)) *(mod_mass_up_low<np.percentile(mod_mass_up_low,70)) #* (sim_mass_up_low>0.15) *(mod_mass_up_low>0.15)
 plt.hist(smass_match[eff_bool])
-plt.xlabel('log(M*) scatter (infer $-$ True): $\pm${0:.2f}'.format(np.std(smass_match[eff_bool])),fontsize=15)
+plt.xlabel('log(M*) scatter (infer $-$ True): {0:.2f}$\pm${1:.2f}'.format(np.mean(smass_match[eff_bool]), np.std(smass_match[eff_bool])),fontsize=15)
 plt.title("filter set: "+comb, fontsize=15)
 plt.show()
 print(folder)
-print(np.std(smass_match))
+print(len(smass_match[eff_bool])/len(smass_match))
+print(len(smass_match))
+
+# plt.hist(sim_mass_up_low[sim_mass_up_low<np.percentile(sim_mass_up_low,60)])
 
 # plt.figure(figsize=(7,5))
 # plt.hist(age_match)
