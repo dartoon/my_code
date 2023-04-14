55486403af5af2048570186952461ae14b7fd362
dartoon <dingxuheng@mail.bnu.edu.cn>
2023-04-14 11:22:28 +0900

    Complete the fitting task

--- projects/2022_COSMOSweb/2d_sed_Tomoko/3_trans_txt_to_image.py
+++ projects/2022_COSMOSweb/2d_sed_Tomoko/3_trans_txt_to_image.py
@@ -14,11 +14,19 @@ import matplotlib.pyplot as plt
 import pickle
 from matplotlib.colors import LogNorm     
 
+def scale_bar(ax, d, dist=1/0.13, text='1"', text2=None, color='black', flipped=False, fontsize=20):
+    p0 = d / 7.
+    ax.plot([p0, p0 + dist], [p0, p0], linewidth=2, color=color)
+    ax.text(p0 + dist / 2., p0 + 0.02 * d, text, fontsize=fontsize, color=color, ha='center')
+    if text2 is not None:
+        ax.text(p0 + dist / 2., p0 - 0.08 * d, text2, fontsize=fontsize, color=color, ha='center')
+        
+
 f = open('fmos_alma_cosmosweb.cat','r')
 string = f.read()
 lines = string.split('\n')
 lines = [lines[i] for i in range(len(lines)) if 'FMOS_J09' in lines[i]]
-idx = 1
+idx = 4
 
 target_name, RA, Dec, z, best_mass = lines[idx].split(' ')
 name = target_name[7:12]
@@ -41,15 +49,15 @@ for ct, line in enumerate(lines[1:-1]):
     if len(line.split(' ')) < 4:
         continue
     else:
-        count, smass, sfr, m_age, l_age, AV, Ebv = line.split(' ')
+        count, smass, sfr, m_age, l_age, AV = line.split(' ')
         count = int(count[4:])
         _i, _j = sed_2d_info[count][0], sed_2d_info[count][1]
         smass_image[_i, _j] = float(smass)    #smass in logMsun
         sfr_image[_i, _j] = float(sfr)          #logMsun/yr 
         age_image[_i, _j] = 10**float(m_age)    #logGyr to Gry
         AV_image[_i, _j] = AV    #logGyr
-        Ebv_image[_i, _j] = Ebv    #E(B-V)
-        Rv.append(float(AV)/float(Ebv))
+        # Ebv_image[_i, _j] = Ebv    #E(B-V)
+        # Rv.append(float(AV)/float(Ebv))
 
 # for i in range(len(smass_image)):
 #     for j in range(len(smass_image)):
@@ -64,18 +72,34 @@ for ct, line in enumerate(lines[1:-1]):
         
 #%%
 import pickle 
+import matplotlib as mat
+mat.rcParams['font.family'] = 'STIXGeneral'
 from galight.tools.astro_tools import plt_fits_color
+from astropy.cosmology import FlatLambdaCDM
+from astropy.visualization import make_lupton_rgb
 image_list = pickle.load(open('colorimage_bin2_{0}.pkl'.format(t_name),'rb'))
+
 images = []
-# zp_list = []
-for i in [-1,-3,-4]:  #['F115W', 'F150W','F277W', 'F444W']
-    # zp_list.append(zp_dict[filts[i]])
+use_filt = ''
+use_filt_id = [4, 3, 1]
+filts = ['F814W','F115W', 'F150W','F277W', 'F444W']
+for i in use_filt_id:  #['ACS','F115W', 'F150W','F277W', 'F444W']
     images.append(image_list[i])
-from galight.tools.astro_tools import plt_fits_color, plt_fits
-# images = [images[i] * 10 ** (-0.4*(zp_list[i]-zp_list[0])) for i in range(len(images)) ]
-# for i in range(len(images)):
-#     plt_fits(images[i], vmin=0.001, vmax=2.5)
-plt_fits_color(images, Q=7, stretch=0.3)
+    use_filt += filts[i] + '+'
+use_filt = use_filt[:-1]
+deltaPix = 0.03*2
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+scale_relation = cosmo.angular_diameter_distance(z).value * 10**3 * (1/3600./180.*np.pi)  #Kpc/arc
+kpc_per_pixel = scale_relation 
+scale = 0.5 * kpc_per_pixel
+rgb_default = make_lupton_rgb(images[0], images[1], images[2], Q=7, stretch=0.3)
+fig, ax = plt.subplots()
+plt.title(t_name)
+plt.imshow(rgb_default, origin='lower')
+plt.text(1,80,use_filt,fontsize=20, color = 'white')
+scale_bar(ax, len(images[0]), dist=0.5/deltaPix, text='0.5"', text2 ='~{0:.2f}kpc'.format(scale), color = 'white')
+plt.savefig('products/color_image_{0}.pdf'.format(t_name))
+plt.show()
 
 
 norm = None  
@@ -85,6 +109,8 @@ plt.title(t_name)
 plt.imshow(smass_image, norm=norm, origin='lower' ) 
 plt.colorbar()
 plt.show()
+print(np.log10(np.sum(10**smass_image)))
+print("BestMass", best_mass)
 
 # print('sfr_image')
 # norm = LogNorm(vmin=0.003, vmax=0.1)#np.max(img[~np.isnan(img)]))
@@ -124,7 +150,8 @@ plt.colorbar()
 plt.show()
 pickle.dump(AV_image , open('Av_{0}.pkl'.format(name), 'wb'))
 
-
+pyfits.PrimaryHDU(smass_image).writeto('products/2D_logsmass_{0}.fits'.format(t_name),overwrite=True)
+pyfits.PrimaryHDU(AV_image).writeto('products/2D_Av_{0}.fits'.format(t_name),overwrite=True)
 
 # import matplotlib
 # cmap_r = matplotlib.cm.get_cmap('RdBu_r')
